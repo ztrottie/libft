40d7a5a826771b4d096a3ac0f9bfd15491333bb5
Zackary Trottier <ztrottie@c2r5p3.42quebec.com>
2022-11-11 00:27:07 -0500

    lol

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ztrottie <zakytrottier@hotmail.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 23:08:26 by ztrottie          #+#    #+#             */
-/*   Updated: 2022/11/10 23:51:12 by ztrottie         ###   ########.fr       */
+/*   Updated: 2022/11/11 00:26:38 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,4 @@ static size_t	ft_last_print(char const *s1)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*ptr;
-	size_t	restart;
-	size_t	i;
-	
-	if (!s1)
-		return (0);
-	ptr = (char *)malloc(ft_strlen(s1));
-	if (!ptr)
-		return (0);
-	i = 0;
-	while (!ft_isprint(s1[i]))
-	{
-		ptr[i]
-	}
 }
